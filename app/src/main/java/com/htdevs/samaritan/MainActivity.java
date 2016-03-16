59ebe7a93ead777f46f782f47f753726d2b800c1
htopiwala <husaint21@gmail.com>
2016-03-16 09:18:20 +0530

    Adding code to handle user's request

--- app/src/main/java/com/htdevs/samaritan/MainActivity.java
+++ app/src/main/java/com/htdevs/samaritan/MainActivity.java
@@ -4,20 +4,92 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ListView messageList;
+    private MessageAdapter messageAdapter;
+    private EditText messageBodyField;
+    private String messageBody;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate(), Inside method");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Log.d(TAG, "onCreate(), setting the ListView with the messageAdapter");
+        messageList = (ListView) findViewById(R.id.listMessages);
+        messageAdapter = new MessageAdapter(this);
+        messageList.setAdapter(messageAdapter);
+
+        messageBodyField = (EditText) findViewById(R.id.messageBodyField);
+
+        //Onclick Listener for the send button
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
     }
 
+
+    //Populate message history
+    private void populateDummyData(){
+        Log.d(TAG, "populateDummyData(), adding dummy data and calling addMessage()");
+        //Data stub till database is added
+        String dummyData[] = {"Hey","Test data", "Working", "Yes its, Working", "Add more data", "And more data. Let's make this data long", "Okay"};
+        for (int i = 0; i < dummyData.length; i++) {
+            String message = dummyData[i];
+            if (i%2==0) {
+                messageAdapter.addMessage(message, MessageAdapter.DIRECTION_INCOMING);
+            } else {
+                messageAdapter.addMessage(message, MessageAdapter.DIRECTION_OUTGOING);
+            }
+        }
+    }
+
+    //Function to add a message to the UI
+    private void sendMessage(){
+        Log.d(TAG,"sendMessage(), send messages to the UI");
+        messageBody = messageBodyField.getText().toString();
+        if(messageBody.isEmpty()){
+            Toast.makeText(this, "Please enter a message!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d(TAG, "sendMessage(), using addMessage to add it to the UI");
+        messageAdapter.addMessage(messageBody, MessageAdapter.DIRECTION_INCOMING);
+        messageBodyField.setText("");
+        handleResponse();
+    }
+
+    //Function to handle the response based on the user request
+    private void handleResponse(){
+        Log.d(TAG, "handleResponse() handle the response of the user's request");
+        String message;
+        if(messageBody.equals("#help")){
+            message = "You can access the following features:\n#twitter\n#weather\n#movies";
+        } else if(messageBody.equals("#movies")){
+            message = "#movies will allow you to search latest movies from the online repository.";
+        }else if(messageBody.equals("#weather")){
+            message = "#weather will allow you to search weather information about a location.";
+        }else if(messageBody.equals("#twitter")){
+            message = "#twitter will give you the latest tweets from a an user account.";
+        }else {
+            message = "You've entered a wrong command, send #help for more information";
+        }
+        messageAdapter.addMessage(message, MessageAdapter.DIRECTION_OUTGOING);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/htdevs/samaritan/MessageAdapter.java
@@ -0,0 +1,97 @@
+package com.htdevs.samaritan;
+
+import android.app.Activity;
+import android.nfc.Tag;
+import android.util.Log;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by intel on 3/15/2016.
+ */
+public class MessageAdapter extends BaseAdapter {
+
+    private static final String TAG = MessageAdapter.class.getSimpleName();
+
+    //Integer values for Incoming and Outgoing function
+    public static final int DIRECTION_INCOMING = 0;
+    public static final int DIRECTION_OUTGOING = 1;
+
+    private List<Pair<String, Integer>> messages;
+    private LayoutInflater layoutInflater;
+
+    //Initializing constructor
+    public MessageAdapter(Activity activity){
+        Log.d(TAG, "MessageAdapter() constructor, initializing layoutInflater");
+        layoutInflater = activity.getLayoutInflater();
+        messages = new ArrayList<Pair<String, Integer>>();
+    }
+
+    //Function to add new message
+    public void addMessage(String message, int direction) {
+        Log.d(TAG, "addMessage(), adding message "+message+" with direction "+direction);
+        messages.add(new Pair(message, direction));
+        notifyDataSetChanged();
+    }
+
+    //Overriding default functions of BaseAdapter
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return messages.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return messages.get(position).second;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        int direction = getItemViewType(position);
+
+        //show message on left or right, depending on if
+        //it's incoming or outgoing
+        if (convertView == null) {
+            int res = 0;
+            if (direction == DIRECTION_INCOMING) {
+                res = R.layout.message_right;
+            } else if (direction == DIRECTION_OUTGOING) {
+                res = R.layout.message_left;
+            }
+            convertView = layoutInflater.inflate(res, parent, false);
+        }
+
+        String message = messages.get(position).first;
+
+        Date date = new Date();
+        TextView txtMessage = (TextView) convertView.findViewById(R.id.txtMessage);
+        TextView txtDate = (TextView) convertView.findViewById(R.id.txtDate);
+        txtDate.setText(date.toString());
+        txtMessage.setText(message);
+
+        return convertView;
+    }
+}
